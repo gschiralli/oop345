6f2216a0d0355e629ce0f1230bb648a0f0d55aac
Gianni Schiralli <gschiralli@hotmail.com>
2021-09-16 16:08:21 -0400

    part1

--- WS01/Event.cpp
+++ WS01/Event.cpp
@@ -1,9 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
 #include "Event.h"
 
-sdds::Event::Event()
-{
-}
 
-void sdds::Event::display() const
-{
+
+size_t g_sysClock = 0;
+namespace sdds {
+	sdds::Event::Event() :time{0}
+	{
+		desc[0] = '\0';
+	}
+	void sdds::Event::display() const
+	{
+		static int counter = 1;
+		int hh, mm, ss = 0;
+		hh = time / 3600;
+		mm = (time / 60) % 60;
+		ss = time % 60;
+		if (desc[0] == '\0')
+		{
+			std::cout.width(2);
+			std::cout << counter;
+			std::cout << ". | No event |" << std::endl;
+
+		}
+		else {
+			std::cout.width(2);
+			std::cout << counter;
+			std::cout<< ". ";
+			std::cout.width(2);
+			std::cout.fill(0);
+			std::cout << hh << ":";
+			std::cout.width(2);
+			std::cout.fill(0);
+			std::cout << mm << ":";
+			std::cout.width(2);
+			std::cout.fill(0);
+			std::cout << ss << "=> " << desc << std::endl;
+
+		}
+
+		counter++;
+
+	}
+
+	void sdds::Event::set(const char* ch)
+	{
+		if (ch) {
+			strncpy(desc, ch, 120);
+		}
+		else
+		{
+			time = 0;
+			desc[0] = '\0';
+		}
+	}
 }
--- WS01/Event.h
+++ WS01/Event.h
@@ -1,16 +1,16 @@
 #ifndef EVENT_H
 #define EVENT_H
-
+extern size_t g_sysClock;
 namespace sdds {
-	extern size_t g_sysClock;
+	
 	class Event {
-		char* description{nullptr};
-		size_t time{0};
+		char desc[120];
+		size_t time{ 0 };
 	public:
 		Event();
 		void display()const;
-		void set(const char* )
-			
+		void set(const char* ch=nullptr);
+
 
 
 	};
