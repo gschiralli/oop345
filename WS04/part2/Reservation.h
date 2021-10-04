972276bdd2b3681d037c3c9559ab0d9e5c550e2e
Gianni Schiralli <gschiralli@hotmail.com>
2021-10-04 18:48:42 -0400

    ws

---
+++ WS04/part2/Reservation.h
@@ -0,0 +1,26 @@
+#ifndef SDDS_RESERVATION_H
+#define SDDS_RESERVATION_H
+
+
+#include <string>
+
+namespace sdds {
+	
+	class Reservation
+	{
+		std::string m_id{};
+		std::string m_name{};
+		std::string m_email{};
+		int m_noOfPeople{};
+		unsigned int m_day{};
+		unsigned int m_time{};
+	public:
+		Reservation();
+		void update(int day, int time);
+		Reservation(const std::string& res);
+
+
+
+	};
+}
+#endif // !SDDS_RESERVATION_H
